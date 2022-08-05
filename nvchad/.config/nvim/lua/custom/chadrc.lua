e133e4e54f21b508ea417b2c28ddf87f4664844f
Javier Luna Molina <javierlunamolina@gmail.com>
2022-08-05 17:23:20 +0200

    Add .env file to global gitignore. Add mason config to nvchad.

--- nvchad/.config/nvim/lua/custom/chadrc.lua
+++ nvchad/.config/nvim/lua/custom/chadrc.lua
@@ -10,7 +10,8 @@ M.plugins = {
 	user = userPlugins,
 	override = {
 		["nvim-treesitter/nvim-treesitter"] = require("custom.plugins.treesitter").overrides,
-		["kyazdani42/nvim-tree.lua"] = require("custom.plugins.treesitter").overrides,
+		["kyazdani42/nvim-tree.lua"] = require("custom.plugins.nvim-tree").overrides,
+		["williamboman/mason.nvim"] = require("custom.plugins.mason").overrides,
 	},
 	options = {
 		lspconfig = {

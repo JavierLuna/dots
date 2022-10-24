eaee661a6a6c3742e283ccaac718742266d5abc3
Javier Luna Molina <jlunamol@amazon.com>
2022-10-24 16:14:40 +0000

    NVChad: Diagnosis in floating window

--- nvchad/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvchad/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -11,9 +11,38 @@ local servers = {
 	"tsserver",
 	"vimls",
 }
+
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+local border = {
+      {"┏", "FloatBorder"},
+      {"━", "FloatBorder"},
+      {"┓", "FloatBorder"},
+      {"┃", "FloatBorder"},
+      {"┛", "FloatBorder"},
+      {"━", "FloatBorder"},
+      {"┗", "FloatBorder"},
+      {"┃", "FloatBorder"},
+}
+
+-- To instead override globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
 	})
 end
+
+vim.diagnostic.config({
+  virtual_text = false,
+})
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
